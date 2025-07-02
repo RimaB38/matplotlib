9364c2073f93d96de1b7762d2ee351a789f68a2b
RimaB38 <r.masirkaite@gmail.com>
2025-07-02 14:38:09 +0300

    Initial commit matplotlib

---
+++ main.py
@@ -0,0 +1,19 @@
+from matplotlib import pyplot as plt
+
+# plt.subplots()
+# plt.show()
+
+
+
+# x reiksmes
+x= [1,2,3,4,5,6,7,8,9]
+a = 30
+x2 =[i*i for i in x]
+x3 = [i**3 for i in x]
+xa = [i**a for i in x]
+fig,(ax1,ax2,ax3) = plt.subplots(1,3)
+ax1.plot(x,x2,color="red",linestyle="-",marker="o")
+ax2.plot(x,x3,color="blue",linestyle="-",marker="x")
+ax3.plot(x,xa,color="purple",linestyle="-",marker="s")
+plt.legend()
+plt.show()
