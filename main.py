3db8f026918bd338e6baf6b82254d395c805dddb
RimaB38 <r.masirkaite@gmail.com>
2025-07-02 15:16:39 +0300

    matplotlib commit d

--- main.py
+++ main.py
@@ -17,3 +17,26 @@ ax2.plot(x,x3,color="blue",linestyle="-",marker="x")
 ax3.plot(x,xa,color="purple",linestyle="-",marker="s")
 plt.legend()
 plt.show()
+
+x =[1,2,3,4,5]
+y1 = [2,2,0,0,2]
+y2 = [4,3,2,1,-1]
+y3 = [2,4,9,16,25]
+y4 = [-1,1,-1,1,-1]
+fig,axs = plt.subplots(2,2,figsize=(10,6))
+
+axs[0,0].plot(x,y1,color="red")
+axs[0,0].plot.set_title("x ir y1(linija)")
+
+axs[0,1].scatter(x,y2,color='green')
+axs[0,1].set_tittle("x,y2,(taskai)")
+
+axs[1,0].plot(x,y3,color='purple',marker='o')
+axs[1,0].set_title("x ir y3,(linija + taskai)")
+
+axs[1,1].plot(x,y4,color='black',linestyle='--')
+axs[1,1].set_title("x,y4,(bruksnine linija)")
+
+plt.tight_layout()
+plt.show()
+
